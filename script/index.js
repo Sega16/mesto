66a3e76dca6b723d790d1eeaaff050f1f9c1e682
Sergey Polozov <fiyzor-99@yandex.ru>
2022-01-16 13:13:21 +0500

    Черновик

---
+++ script/index.js
@@ -0,0 +1,45 @@
+let content = document.querySelector('.content');
+let profile = content.querySelector('.profile');
+
+let btnEdit = profile.querySelector('.profile__btn-edit');
+let profileName = profile.querySelector('.profile__title');
+let profileAdout = profile.querySelector('.profile__subtitle');
+
+let popup = document.querySelector('.popup');
+let popupClose = popup.querySelector('.popup__close');
+let nameInput = popup.querySelector('.popup__name');
+let aboutInput = popup.querySelector('.popup__about');
+let popupBtnSave = popup.querySelector('.popup__btn')
+
+let cards = content.querySelector('.cards');
+let card = cards.querySelector('.card');
+let likeBtn = cards.querySelectorAll('div.card .card__like');
+
+function likeActive() {
+    let cardName = card.querySelector('.card__name');
+    cardName.innerHTML = 
+    `<p class="card__text">Карачаевск</p>
+    <img class="card__like link" src="./images/Like_img_active.svg" alt="лайк карточки">`
+}
+likeBtn.addEventListener('click', likeActive);
+
+function PopupOpened() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileAdout.textContent;
+}
+btnEdit.addEventListener('click', PopupOpened);
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileAdout.textContent = aboutInput.value;
+}
+popupBtnSave.addEventListener('submit', formSubmitHandler);
+popupBtnSave.addEventListener('click', closePopup);
+popupBtnSave.addEventListener('click', formSubmitHandler);
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+popupClose.addEventListener('click', closePopup);
